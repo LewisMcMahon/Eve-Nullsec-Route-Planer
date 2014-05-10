2f9e4248d6b003bf0a8f11593ae9c8c114b6bf75
Lewis McMahon <Lewis@lewismcmahon.co.uk>
2014-05-10 18:07:20 +0100

    converted the code to a function that returns an array of the route

---
+++ index.php
@@ -0,0 +1,11 @@
+<?
+include ("route/route.php");
+
+$route = getRoute(30000142,30002187);
+
+print_r($route);
+
+
+
+
+?>
---
+++ route/db.inc.php
@@ -0,0 +1,1 @@
+<?php $dbh = new PDO('mysql:host=localhost;dbname=sde;charset=utf8', 'root', ''); ?>
